9403752e0f77aec44d0a0973d746623d78100a8d
SolsticeLeaf <me@sleaf.dev>
2025-09-22 21:25:55 +0300

    fixed settings

--- src/rust/ru.readmanga/src/helpers.rs
+++ src/rust/ru.readmanga/src/helpers.rs
@@ -1,8 +1,8 @@
 use aidoku::{
-    error::{AidokuError, AidokuErrorKind, NodeError, Result},
-    prelude::*,
-    std::{defaults::defaults_get, net::{HttpMethod, Request}},
-    Manga, MangaPageResult,
+	error::{AidokuError, AidokuErrorKind, NodeError, Result},
+	prelude::*,
+	std::net::{HttpMethod, Request},
+	Manga, MangaPageResult,
 };
 use alloc::{string::String, vec::Vec};
 
@@ -10,18 +10,6 @@ use crate::{
 	constants::{BASE_URL, SEARCH_OFFSET_STEP},
 	wrappers::WNode,
 };
-
-pub fn get_base_url() -> String {
-    defaults_get("baseUrl")
-        .and_then(|v| v.as_string().ok())
-        .map(|s| s.read())
-        .filter(|s| !s.is_empty())
-        .unwrap_or_else(|| BASE_URL.to_string())
-}
-
-pub fn get_search_base_url() -> String {
-    format!("{}/search/advancedResults?", get_base_url())
-}
 
 pub fn get_html(url: &str) -> Result<WNode> {
 	Request::new(url, HttpMethod::Get)
@@ -31,7 +19,7 @@ pub fn get_html(url: &str) -> Result<WNode> {
 }
 
 pub fn get_manga_url(id: &str) -> String {
-    format!("{}/{}", get_base_url(), id)
+	format!("{}/{}", BASE_URL, id)
 }
 
 pub fn create_manga_page_result(mangas: Vec<Manga>) -> MangaPageResult {
@@ -44,7 +32,7 @@ pub fn create_manga_page_result(mangas: Vec<Manga>) -> MangaPageResult {
 
 pub fn get_chapter_url(manga_id: &str, chapter_id: &str) -> String {
 	// mtr is 18+ skip
-    format!("{}/{manga_id}/{chapter_id}?mtr=true", get_base_url())
+	format!("{BASE_URL}/{manga_id}/{chapter_id}?mtr=true")
 }
 
 pub fn create_parsing_error() -> AidokuError {
