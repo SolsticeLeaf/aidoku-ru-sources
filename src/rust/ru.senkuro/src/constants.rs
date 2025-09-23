9c758fafca9c121e740f246cce0457a9242357f3
SolsticeLeaf <me@sleaf.dev>
2025-09-23 13:11:14 +0300

    Senkuro removed

--- src/rust/ru.senkuro/src/constants.rs
+++
@@ -1,6 +0,0 @@
-use const_format::formatcp;
-
-pub const BASE_URL: &str = "https://senkuro.me";
-pub const BASE_SEARCH_URL: &str = formatcp!("{}/{}", BASE_URL, "browse/manga/");
-
-pub const SEARCH_OFFSET_STEP: i32 = 50;
